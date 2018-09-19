ed5fe58e03872910caf526562c0c78bb086a0c63
Your NameAftan <aftan.merrida@gmail.com>
2018-09-18 20:18:36 -0500

    card

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ const notesInputElem = document.getElementById('notesInput');// type your id's//
 
 const submitToDoInputButton = document.getElementById('submitToDoInputButton');
 
+const activateDeletes =() => {
+    const deleteButtons =document.getElementsByClassName('deleteButtons');
+
+for (let i = 0; i < deleteButtons.length; i++){
+    const element = deleteButtons[i];
+    element.addEventListener("click", (e) => {
+        //card that the button was on
+        const buttonIclicked = e.target;
+        const cardTodoDelete = buttonIclicked.parentNode.parentNode;
+        cardTodoDelete.remove();
+        //.remove()
+        //.parentNode
+        
+        console.log ("they clicked delete!!");
+    })
+}
+}
+
 const printToDom =(stringToPrint, whereToPrint) => {
     document.getElementById(whereToPrint).innerHTML += stringToPrint;
 }
@@ -12,12 +30,16 @@ const buildNewToDoCard = (toDo, notes) => {
     <div class="card-body">
       <h5 class="card-title">${toDo}</h5>
       <p class="card-text">${notes}.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <button href="#" class="btn btn-primary deleteButtons">Delete this</button>
     </div>
-  </div>`
+  </div>`;
 
     printToDom(DOMString, 'toDoCards');
+    //this is calling the delete button//
+    activateDeletes()
+    
 }
+
 //now add the event listener itself//
 
 submitToDoInputButton.addEventListener("click", (e) => {
